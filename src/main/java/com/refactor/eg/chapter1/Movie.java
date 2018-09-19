8f093cf9d004a605536afe1235df51352801a10a
Vivek <vivekrmd@gmail.com>
2018-09-19 10:09:49 +0530

    Move method/responsibility to the appropriate class

--- src/main/java/com/refactor/eg/chapter1/Movie.java
+++ src/main/java/com/refactor/eg/chapter1/Movie.java
@@ -24,5 +24,31 @@ public class Movie {
 
 	public String getTitle() {
 		return _title;
-	};
+	}
+
+	public double getCharge(int daysRented) {
+		double amount = 0;
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			amount += 2;
+			if (daysRented > 2)
+				amount += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			amount += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			amount += 1.5;
+			if (daysRented > 3)
+				amount += (daysRented - 3) * 1.5;
+			break;
+		}
+		return amount;
+	}
+
+	public int getFrequentRenterPoints(int daysRented) {
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			return 2;
+		return 1;
+	}
 }
--- src/main/java/com/refactor/eg/chapter1/Rental.java
+++ src/main/java/com/refactor/eg/chapter1/Rental.java
@@ -16,30 +16,12 @@ public class Rental {
 	public Movie getMovie() {
 		return _movie;
 	}
-	
+
 	public double getCharge() {
-		double amount = 0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			amount += 2;
-			if (getDaysRented() > 2)
-				amount += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			amount +=getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			amount += 1.5;
-			if (getDaysRented() > 3)
-				amount += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return amount;
+		return _movie.getCharge(getDaysRented());
 	}
-	
+
 	public int getFrequentRenterPoints() {
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-			return 2;
-		return 1;
+		return _movie.getFrequentRenterPoints(getDaysRented());
 	}
 }
