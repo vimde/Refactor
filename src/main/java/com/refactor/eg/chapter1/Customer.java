c137008032fbcd62f9a2622d0da1a24985507ccc
Vivek <vivekrmd@gmail.com>
2018-09-19 10:00:33 +0530

    Replace Temporary variables with Query - performance is being questioned

--- src/main/java/com/refactor/eg/chapter1/Customer.java
+++ src/main/java/com/refactor/eg/chapter1/Customer.java
@@ -20,26 +20,39 @@ class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			
-			Rental each = (Rental) rentals.nextElement();
-
-			// add frequent renter points
-			frequentRenterPoints += each.getFrequentRenterPoints();
-
+			Rental each = rentals.nextElement();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points";
 		return result;
+	}
+
+	private int getFrequentRenterPoints() {
+		int frequentRenterPoints = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            frequentRenterPoints += each.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
+	}
+
+	private double getTotalCharge() {
+		double totalAmount = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            totalAmount += each.getCharge();
+        }
+        return totalAmount;
 	}
 
 }
