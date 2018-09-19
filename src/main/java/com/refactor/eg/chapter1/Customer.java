113eb0075b8de1d74eba5ac9387c732004b15f77
Vivek <vivekrmd@gmail.com>
2018-09-19 09:05:03 +0530

    Move method (from Customer to Rental)

--- src/main/java/com/refactor/eg/chapter1/Customer.java
+++ src/main/java/com/refactor/eg/chapter1/Customer.java
@@ -49,22 +49,6 @@ class Customer {
 	}
 
 	private double amountFor(Rental rental) {
-		double amount = 0;
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			amount += 2;
-			if (rental.getDaysRented() > 2)
-				amount += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			amount += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			amount += 1.5;
-			if (rental.getDaysRented() > 3)
-				amount += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return amount;
+		return rental.getCharge();
 	}
 }
