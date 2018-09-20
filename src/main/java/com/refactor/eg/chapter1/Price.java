326581439436e2ff80037ec6c8ed2dbb5caa1e42
Vivek <vivekrmd@gmail.com>
2018-09-20 09:34:57 +0530

    Made the method abstract after overriding in subclasses - price

--- src/main/java/com/refactor/eg/chapter1/Price.java
+++ src/main/java/com/refactor/eg/chapter1/Price.java
@@ -3,23 +3,5 @@ package com.refactor.eg.chapter1;
 public abstract class Price {
 	public abstract int getPriceCode();
 
-	public double getCharge(int daysRented) {
-		double amount = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			amount += 2;
-			if (daysRented > 2)
-				amount += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			amount += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			amount += 1.5;
-			if (daysRented > 3)
-				amount += (daysRented - 3) * 1.5;
-			break;
-		}
-		return amount;
-	}
+	public abstract double getCharge(int daysRented);
 }
